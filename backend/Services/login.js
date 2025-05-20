bd6d6dfe53ef5b956850e4e1a4c9ac6f46efac00
Julieta Laspiur <julieta.laspiur771@gmail.com>
2025-05-20 18:26:31 -0300

    modificación services login.js

--- backend/Services/login.js
+++ backend/Services/login.js
@@ -1,24 +1,25 @@
-export async function loginService(credentials){
-if(!credentials || !credentials.username || !credentials.password || typeof credentials.username != 'string' || typeof credentials.password != 'string'){ 
-    return{
-        error:'Argumentos no válidos'
-    } 
-    //throw new InvalidArgumentException();
+import { InvalidArgumentException } from "../exceptions/invalid_argument_exceptions.js";
+import { InvalidCredentialsException } from "../exceptions/invalid_credentials_exceptions.js";
 
-}
-    if(credentials.username != 'admin'){
-        return{
-            error: 'credenciales no valida',
-            
-        };
-    }
+export class LoginService {
+static async login(credentials){
+    if(!credentials 
+        || !credentials.username 
+        || !credentials.password 
+        || typeof credentials.username != 'string' 
+        || typeof credentials.password != 'string'
+    )
+    throw new InvalidArgumentException();
 
-    if(credentials.password != '1234'){
-        return{
-            error: 'Credencial inválida'
-        };
-    }
- return {
+    if(credentials.username != 'admin')
+        throw new InvalidCredentialsException();
+    
+
+    if(credentials.password != '1234')
+        throw new InvalidCredentialsException();
+    
+     return {
     Token:'Token de acceso'
- };
+  };
+ }
 }
