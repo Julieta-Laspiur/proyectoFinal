ba93603bfa31d0c92e6c19d5acc4555cb64f02fe
Julieta Laspiur <julieta.laspiur771@gmail.com>
2025-06-17 10:02:23 -0300

    Creación y eliminación de usuarios.

--- backend/Services/login.js
+++ backend/Services/login.js
@@ -1,25 +1,47 @@
 import { InvalidArgumentException } from "../exceptions/invalid_argument_exceptions.js";
 import { InvalidCredentialsException } from "../exceptions/invalid_credentials_exceptions.js";
+import { getDependency } from "../libs/dependencies.js";
+import bcrypt from 'bcrypt';
+import config from '../config.js';
+import jwt from 'jsonwebtoken';
 
 export class LoginService {
-static async login(credentials){
+  static async login(credentials){
     if(!credentials 
-        || !credentials.username 
-        || !credentials.password 
-        || typeof credentials.username != 'string' 
-        || typeof credentials.password != 'string'
+      || !credentials.username 
+      || !credentials.password 
+      || typeof credentials.username != 'string' 
+      || typeof credentials.password != 'string'
     )
     throw new InvalidArgumentException();
 
-    if(credentials.username != 'admin')
-        throw new InvalidCredentialsException();
-    
+    const UserService= getDependency('UserService');
+    const user = await UserService.getSingleOrNullByUsername(credentials.username);
+    if(!user)
+      throw new InvalidCredentialsException();
 
-    if(credentials.password != '1234')
-        throw new InvalidCredentialsException();
-    
-     return {
-    Token:'Token de acceso'
-  };
- }
+    /*console.log('Calculando hash');
+      const hash = await bcrypt.hash ('1234', 2);
+      console.log(hash);
+      console.log('Hash calculado');    
+    */
+
+    if (!(await bcrypt.compare(credentials.password, user.hashPassword)))
+      throw new InvalidCredentialsException();
+
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        username: user.username,
+        fullName: user.fullName,
+        roles: user.roles,
+      },
+      config.jwtKey,
+        {
+         expiresIn : '24h' //El token expirará en 1 día
+        }
+      );
+
+    return{token };
+  }
 }
---
+++ backend/controllers/user.js
@@ -0,0 +1,38 @@
+import { UserService } from '../Services/user.js';
+import { checkForRole } from '../middlewares/authorization_middleware.js';
+
+export function user (app) {
+    app.get(
+        '/user',
+        checkForRole('admin'),
+        async (req res) => {
+            const users = await UserService.get();
+            const result = users.map(user => ({
+                uuid: user.uuid,
+                username: user.username,
+                fullName: user.fullName,
+                email: useReducer.email,
+                roles: user.roles,
+            }));
+
+            res.send(result);
+        }
+    );
+    app.post(
+        '/user',
+        checkForRole('admin'),
+        async (req, res) => {
+            await UserService.create(req.body);
+            res.status(204).send();
+        }
+    );
+
+    app.delete(
+        '/user/:uuid',
+        checkForRole('admin'),
+        async (req, res) => {
+            await UserService.deleteByUuid(req.parms.uuid);
+            res.status(204).send();
+        }
+    );
+}
